2a99945263a0e5b8bb82f505e520c87903baaac0
axeldotdev <axel.charpentier@hey.com>
2021-10-02 15:49:04 +0200

    improve footer with social links

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,11 @@
 <footer aria-labelledby="footer-heading">
     <h2 id="footer-heading" class="sr-only">Footer</h2>
     <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-        <p class="text-base text-gray-400 xl:text-center">
-            &copy; 2021 Axeldotdev
+        <p class="text-base text-gray-500 dark:text-gray-400 xl:text-center">
+            &copy; 2021 Axeldotdev &mdash; Follow me on <a href="https://twitter.com/axeldotdev"
+                class="font-sans font-medium text-gray-800 dark:text-gray-50 underline">Twitter</a>
+            and on <a href="https://github.com/axeldotdev"
+                class="font-sans font-medium text-gray-800 dark:text-gray-50 underline">Github</a>.
         </p>
     </div>
 </footer>
